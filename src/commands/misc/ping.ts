b195cc47e0de152c0cfc2d8b61a5b7b4c7230204
Mrlion <mrlion4202@gmail.com>
2024-03-12 19:03:05 +0000

    updated dependencies a added some things

---
+++ src/commands/misc/ping.ts
@@ -0,0 +1,10 @@
+import type { CommandData, SlashCommandProps } from 'commandkit';
+
+export const data: CommandData = {
+  name: 'ping',
+  description: '🏓 Pong!'
+};
+
+export function run({ interaction, client, handler }: SlashCommandProps) {
+  interaction.reply(`:ping_pong: Pong! ${client.ws.ping}ms`);
+}
