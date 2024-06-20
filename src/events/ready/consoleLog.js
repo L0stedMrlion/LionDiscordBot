a2dc74c908740eef9e8dccb734730aca54c89456
Mrlion <mrlion4202@gmail.com>
2024-06-20 17:35:05 +0000

    added status

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,3 +1,14 @@
+const { ActivityType } = require("discord.js");
+
 module.exports = (client) => {
   console.log(`🦁 LionBot is ready to use.`);
+  client.user.setActivity(status[0]);
 };
+
+let status = [
+  {
+    name: "Watching Mrlion’s Stream",
+    type: ActivityType.Streaming,
+    url: "https://www.twitch.tv/lostedmrlion",
+  },
+];
