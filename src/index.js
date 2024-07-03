8229d07a9d2b8fa900a7a79bba76983c23565f7c
Mrlion <mrlion4202@gmail.com>
2024-07-03 09:10:28 +0000

    minor changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const client = new Client({
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
-    IntentsBitField.Flags.GuildBans,
   ],
 });
 
