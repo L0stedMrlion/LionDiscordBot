b61c699cef79690bdd13905114ac3033aa41db16
Mrlion <mrlion4202@gmail.com>
2024-07-03 08:58:01 +0000

    better guide command

--- src/commands/misc/guide.js
+++ src/commands/misc/guide.js
@@ -1,9 +1,29 @@
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
+
 module.exports = {
-    name: "guide",
-    description: "🔗 Sent you link for the guide",
-  
-    callback: (client, interaction) => {
-      interaction.reply(`You can find our guide here on link [guide.lionsproject.eu](https://guide.lionsproject.eu/)`);
-    },
-  };
-  
+  name: "guide",
+  description: "📘 Sent you link for the guide",
+
+  callback: (client, interaction) => {
+    const embed = new EmbedBuilder().setColor("#0099ff").setTitle("📘 Guide")
+      .setDescription(`
+        You can find our guide here on [guide.lionsproject.eu](https://guide.lionsproject.eu/). You can find there many useful informations!
+
+        If you want improve our guide you can make a pull request on our [Github](https://github.com/L0stedMrlion/lionpolicerp-guide).
+      `);
+
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel("🔗 Open Guide")
+        .setStyle(ButtonStyle.Link)
+        .setURL("https://guide.lionsproject.eu/")
+    );
+
+    interaction.reply({ embeds: [embed], components: [row] });
+  },
+};
